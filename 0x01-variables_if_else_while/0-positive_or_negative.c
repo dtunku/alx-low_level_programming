45a937814ae082d9cd4efd68c88c902d01069b44
Khalil <dantunku.developer@gmail.com>
2022-12-10 08:46:34 -0800

    fix task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main(void)
-{
+/**
+ * main - Entry pint
+ * Description - Prints if random generated number is +tive, zero or -tive
+ * Return: Always 0 (Succes) 
+*/
+int main(void){
+
 	int n;
+
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0){
 		printf("%d is positive \n" ,n );
@@ -17,4 +23,5 @@ int main(void)
 	else{
 		printf("%d is negative \n",n)
 	}
+
 	return (0);
