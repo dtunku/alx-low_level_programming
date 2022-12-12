89fa239bd382aa411334d9e31ed426d2ce5b2d19
Khalil <dantunku.developer@gmail.com>
2022-12-12 09:01:05 -0800

    added fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,9 @@
+/**
+ * main -  print out alphabets from a -z
+ * return - zero
+ **/
+
+
 #include <stdio.h>
 
 
@@ -6,18 +12,17 @@ int main(void)
 
 {
 
-   char ch;
+char ch;
 
 
 
-   for (ch = 'a'; ch <= 'z'; ch++)
-  {
-   putchar(ch);
-  }
-					        
-  putchar('\n');
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
 
-  return 0;
+return (0);
 
 }
 
