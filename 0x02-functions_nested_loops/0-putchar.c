36312ed350010de85b89cb311dbf36d9de2faae4
khalil <dantunku.developer@gmail.com>
2022-12-14 18:04:48 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-#include <stdio.h>
 
-int main() {
-  printf("_putchar\n");
-  return 0;
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
+
