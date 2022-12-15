769b027e61b6d734cff810d52cfe8572ef28b56f
Khalil <dantunku.developer@gmail.com>
2022-12-14 21:58:21 -0800

    fix 1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,1 @@
 
-/**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
- **/
-#include <stdio.h>
-int main(void)
-{
-  for (char c = 'a'; c <= 'z'; c++) {
-      putchar(c);
-  }
-  for (char c = 'A'; c <= 'Z'; c++) {
-        putchar(c);
-  }
-  putchar('\n');
-    return 0;
- }
