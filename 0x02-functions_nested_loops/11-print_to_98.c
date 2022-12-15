2d480ca0bc3c3f6805b69e55dd15b83a4b5f7fb9
khalil <dantunku.developer@gmail.com>
2022-12-15 05:20:22 +0100

    fixed 11 and add 100

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,14 +4,15 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
-	 * @n: print from this number
+ * @n: print from this number
  */
 void print_to_98(int n)
 {
 
 	int i, j;
+
 	if (n <= 98)
-	
+
 {
 		for (i = n; i <= 98; i++)
 		{
